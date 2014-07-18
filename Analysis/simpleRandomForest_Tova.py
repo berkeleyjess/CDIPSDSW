b003f326b11610dbcaa7ab0bc82113d41a70c616
trholmes <tova.r.holmes@gmail.com>
2014-07-18 00:14:33 -0700

    Added some time manipulation.

--- Analysis/simpleRandomForest_Tova.py
+++ Analysis/simpleRandomForest_Tova.py
@@ -3,6 +3,9 @@ import numpy as np
 import csv as csv
 from sklearn.ensemble import RandomForestClassifier
 import cPickle
+from pytz import timezone
+
+execfile('time_zones.py')
 
 # Import data files
 with open(r"Data/trainDataFrame.pickle", "rb") as input_file:
@@ -12,17 +15,45 @@ with open(r"Data/testDataFrame.pickle", "rb") as input_file:
 
 # Full list of variables: 'available', 'available_now', 'client', 'id', 'last_impression_time', 'last_seen_online_time', 'lesson_id', 'lesson_subject', 'time_clicked', 'time_clicked_score', 'time_sent_success', 'timezone', 'tutor_id'
 
-# Add my success variable to the training sample
-f = lambda x: False if x==None else True
-train_df['clicked'] = train_df['time_clicked'].map(f)
-test_df['clicked'] = test_df['time_clicked'].map(f)
+
+for df in [train_df, test_df]:
+
+    # Store variables relative to when ping was sent
+    f = lambda x: 0 if x==None else x / np.timedelta64(1, 'h')
+    j = lambda x: 0 if np.isnan(x) or np.isinf(x) else x
+
+    df['online_for'] = df['time_sent_success'] - df['last_seen_online_time']
+    #for of in df['online_for']:
+    #    of = of / np.timedelta64(1, 'h')
+    df['online_for']=df['online_for'].map(f)
+    df['online_for']=df['online_for'].map(j)
+
+    df['last_impression'] = df['time_sent_success'] - df['last_impression_time']
+    #for li in df['last_impression']:
+    #    li = li / np.timedelta64(1, 'h')
+    df['last_impression']=df['last_impression'].map(f)
+    df['last_impression']=df['last_impression'].map(j)
+
+    # Get time in local timezone
+    f = lambda x: 0 if x==None or np.isnan(x) or np.isinf(x) else x
+
+    df['local_time'] = df['time_sent_success']
+    for x in xrange(len(df['timezone'])):
+        tz = timezone(COMMON_TIMEZONES_STR[df['timezone'][x]])
+        df['local_time'][x] = df['time_sent_success'][x].astimezone(tz).hour + df['time_sent_success'][x].astimezone(tz).minute/60.
+    df['local_time'] = df['local_time'].map(f)
+
+    # Add my success variable to the training sample
+    f = lambda x: False if x==None else True
+    df['clicked'] = df['time_clicked'].map(f)
 
 # Store IDs and true answer for output
 ids = test_df['id'].values
 real_click = test_df['clicked'].values
+fit_data = train_df['clicked'].values
 
 # Remove everything I don't care about
-train_df = train_df.drop(['client', 'id', 'last_impression_time', 'last_seen_online_time', 'lesson_id', 'lesson_subject', 'time_clicked', 'time_clicked_score', 'time_sent_success', 'timezone', 'tutor_id'], axis=1)
+train_df = train_df.drop(['clicked','client', 'id', 'last_impression_time', 'last_seen_online_time', 'lesson_id', 'lesson_subject', 'time_clicked', 'time_clicked_score', 'time_sent_success', 'timezone', 'tutor_id'], axis=1)
 
 test_df = test_df.drop(['clicked', 'client', 'id', 'last_impression_time', 'last_seen_online_time', 'lesson_id', 'lesson_subject', 'time_clicked', 'time_clicked_score', 'time_sent_success', 'timezone', 'tutor_id'], axis=1)
 
@@ -30,13 +61,17 @@ test_df = test_df.drop(['clicked', 'client', 'id', 'last_impression_time', 'last
 # Convert back to a numpy array
 train_data = train_df.values
 test_data = test_df.values
-
+'''
 # Note that I need to add the 'clicked' field last
 (nrows, ncols) = train_data.shape
 
+print train_data.shape, test_data.shape
+print ncols-1
+'''
+
 print 'Training...'
 forest = RandomForestClassifier(n_estimators=100)
-forest = forest.fit( train_data[0::,0::ncols-1], train_data[0::,ncols-1] )
+forest = forest.fit( train_data, fit_data )
 
 print 'Predicting...'
 output = forest.predict(test_data)

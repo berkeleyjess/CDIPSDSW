382e53289111a47be2027809c93ea2c5fdc365e1
trholmes <tova.r.holmes@gmail.com>
2014-07-18 00:15:08 -0700

    Changed the output.

--- Analysis/getAccuracy.py
+++ Analysis/getAccuracy.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import csv as csv
 
 results = pd.read_csv('output_guesses.csv', header=0)
@@ -17,6 +16,11 @@ for x in xrange(len(results['id'])):
         if results['will_click'][x]==False: n_notclicked_predicted+=1
         else: n_notclicked_missed+=1
 
-print "Fraction of clicks predicted:", float(n_clicked_predicted)/(n_clicked_missed+n_clicked_predicted)
-print "Fraction of non-clicks predicted:", float(n_notclicked_predicted)/(n_notclicked_missed+n_notclicked_predicted)
+print "------------------------------------------------------"
+print "Fraction of clicks predicted: \t\t\t %.2f" %(float(n_clicked_predicted)/(n_clicked_missed+n_clicked_predicted))
+print "Fraction of non-clicks predicted: \t\t %.2f" %(float(n_notclicked_predicted)/(n_notclicked_missed+n_notclicked_predicted))
+print "------------------------------------------------------"
+print "Fraction of selected tutors that connected: \t %.2f" %(float(n_clicked_predicted)/(n_notclicked_missed+n_clicked_predicted))
+print "Fraction of non-selected tutors that connected:  %.2f" %(float(n_clicked_missed)/(n_clicked_missed+n_notclicked_predicted))
+print "------------------------------------------------------"
 

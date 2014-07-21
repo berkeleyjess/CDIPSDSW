b1f4f5e54ad3ef313e2740a552e09009fa81b7d2
trholmes <tova.r.holmes@gmail.com>
2014-07-21 16:37:32 -0700

    Added structure for analysis.

---
+++ Analysis/cleanData.py
@@ -0,0 +1,1 @@
+# This is the place to do basic cleaning of data -- dealing with nonetypes and NaN as well as timezone processing.
---
+++ Analysis/randomForestAnalysis.py
@@ -0,0 +1,3 @@
+# Chooses the functions from processTools.py it would like to use
+# Applies the RandomForest algorithm and stores the output.
+#   For now, all analysis scripts should output a csv with columns 'ping_id', 'data', 'model'

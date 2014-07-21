366ee587d085f4f9a6e1ac644a33422fad41dc84
trholmes <tova.r.holmes@gmail.com>
2014-07-21 16:39:16 -0700

    Added evalution file.

---
+++ Analysis/evaluation.py
@@ -0,0 +1,1 @@
+# This file should read in the output csv's from Analysis files and comparatively assess them. Much of this will probably be moved into plotting scripts once we're dealing with continuous variables.

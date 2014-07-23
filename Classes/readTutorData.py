bb421aadf9108cc9482dfc71aae69d5d64a29e23
trholmes <tova.r.holmes@gmail.com>
2014-07-23 10:36:05 -0700

    Added hourly average response time values.

--- Classes/readTutorData.py
+++ Classes/readTutorData.py
@@ -6,6 +6,7 @@
 #   hourly_response[hour] = in local timezone, what fraction of pings do tutors respond to within 30 seconds
 #   hourly_response_n_clicked[hour] = gives total number clicked within 30 seconds for each hour
 #   hourly_response_n_not_clicked[hour] = gives number not clicked within 30 seconds for each hour
+#   hourly_average_sec_response[hour] = gives average tutor response rate when they do connect
 
 import pandas as pd
 import numpy as np
@@ -22,7 +23,7 @@ total_pings = len(pings_df['tutor_id'])
 tutor_ids=pings_df['tutor_id'].unique()
 
 # Produce dataframe indexed by tutor_id with attributes above
-print "Building tutor dataframe..."
+print "Getting tutor info..."
 df_dict = {}
 series_dicts = {}
 for tid in tutor_ids:
@@ -42,16 +43,18 @@ for x in xrange(total_pings):
         series_dicts[tid]['n_clicks_under_30']=0
         series_dicts[tid]['hourly_response_n_clicked']=[0]*24
         series_dicts[tid]['hourly_response_n_not_clicked']=[0]*24
+        series_dicts[tid]['hourly_average_sec_response']=[0]*24
 
     # Add number of clicks variables
     if entry['time_clicked'] != None:
         series_dicts[tid]['n_clicks']+=1
+        series_dicts[tid]['hourly_average_sec_response'][int(entry['time_sent_success_local'])]+=entry['sec_response']
     if entry['time_clicked'] != None and entry['sec_response']<30:
         series_dicts[tid]['n_clicks_under_30']+=1
         series_dicts[tid]['hourly_response_n_clicked'][int(entry['time_sent_success_local'])]+=1
     else: series_dicts[tid]['hourly_response_n_not_clicked'][int(entry['time_sent_success_local'])]+=1
 
-print "Processing tutor dataframe..."
+print "Building tutor dataframe..."
 
 for tid in series_dicts.keys():
     # Compute hourly response
@@ -60,6 +63,11 @@ for tid in series_dicts.keys():
         total_this_hour = series_dicts[tid]['hourly_response_n_clicked'][x] + series_dicts[tid]['hourly_response_n_not_clicked'][x]
         if total_this_hour == 0: series_dicts[tid]['hourly_response'].append(-1)
         else: series_dicts[tid]['hourly_response'].append(series_dicts[tid]['hourly_response_n_clicked'][x] / float(total_this_hour))
+
+        # Compute average response time
+        if series_dicts[tid]['hourly_response_n_clicked'][x]!=0:
+            series_dicts[tid]['hourly_average_sec_response'][x] /= float(series_dicts[tid]['hourly_response_n_clicked'][x])
+        else: series_dicts[tid]['hourly_average_sec_response'][x] = -1
 
     # Form series to go into dataframe
     s = pd.Series(series_dicts[tid])

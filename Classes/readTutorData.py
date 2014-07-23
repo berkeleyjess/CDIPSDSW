770cbf7039e762ddb50b1c433b67497f6247b923
trholmes <tova.r.holmes@gmail.com>
2014-07-23 13:33:33 -0700

    Made list of subjects tutored.

--- Classes/readTutorData.py
+++ Classes/readTutorData.py
@@ -7,6 +7,7 @@
 #   hourly_response_n_clicked[hour] = gives total number clicked within 30 seconds for each hour
 #   hourly_response_n_not_clicked[hour] = gives number not clicked within 30 seconds for each hour
 #   hourly_average_sec_response[hour] = gives average tutor response rate when they do connect
+#   subjects = list of subjects taught by tutor
 
 import pandas as pd
 import numpy as np
@@ -44,6 +45,7 @@ for x in xrange(total_pings):
         series_dicts[tid]['hourly_response_n_clicked']=[0]*24
         series_dicts[tid]['hourly_response_n_not_clicked']=[0]*24
         series_dicts[tid]['hourly_average_sec_response']=[0]*24
+        series_dicts[tid]['subjects']=[]
 
     # Add number of clicks variables
     if entry['time_clicked'] != None:
@@ -54,9 +56,16 @@ for x in xrange(total_pings):
         series_dicts[tid]['hourly_response_n_clicked'][int(entry['time_sent_success_local'])]+=1
     else: series_dicts[tid]['hourly_response_n_not_clicked'][int(entry['time_sent_success_local'])]+=1
 
+    # Add list of subjects taught
+    series_dicts[tid]['subjects'].append(entry['lesson_subject'])
+
+
 print "Building tutor dataframe..."
 
 for tid in series_dicts.keys():
+    # Keep only unique subjects
+    series_dicts[tid]['subjects'] = list(set(series_dicts[tid]['subjects']))
+
     # Compute hourly response
     series_dicts[tid]['hourly_response'] = []
     for x in xrange(24):

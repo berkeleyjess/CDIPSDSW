73a808fbe0e2a194874dbf406b67bacf6e4797f7
Michael Mortonson <michael.mortonson@gmail.com>
2014-07-30 18:40:43 -0700

    Clean up evaluation script

--- Analysis/evaluation.py
+++ Analysis/evaluation.py
@@ -1,6 +1,3 @@
-# This file should read in the output csv's from Analysis files and comparatively assess them. Much of this will probably be moved into plotting scripts once we're dealing with continuous variables.
-
-
 """
 Given test data from a ping DataFrame with added data on tutors
 and a model fit to training data:
@@ -40,60 +37,72 @@ wait_time_actual = []
 wait_time_pred = []
 
 print 'Simulating pings and computing statistics...'
-for ping_id, actual, prob in zip(ping_id_headers, actual_headers, prob_headers):
+for i_test, (ping_id, actual, prob) in enumerate(zip(
+        ping_id_headers, actual_headers, prob_headers)):
+    
+    print '  Test subsample', i_test, '/', len(ping_id_headers)
+
     # get useful columns from ping DataFrame
     test_ping_df = ping_df[ping_df.id.isin(pred_df[ping_id].values)]
-    for col in ['lesson_id', 'id', 'time_sent_success', 'time_clicked', 'sec_response', 'sec_since_online']:
+    for col in ['lesson_id', 'id', 'time_sent_success', 
+                'time_clicked', 'sec_response']:
         new_col = test_ping_df[col]
         new_col.index = pred_df[ping_id].index
         pred_df[col] = new_col
-    # compute new actual values
-    #pred_df[actual] = (pred_df.sec_response < 30.)
+
     # group by lesson_id
-    # **** REMOVE HEAD(50) ****
-    for lesson, lesson_df in pred_df.head(1000).groupby('lesson_id', sort=False):
+    grouped_lessons = pred_df.groupby('lesson_id', sort=False)
+    for il, (lesson, lesson_df) in enumerate(grouped_lessons):
+
+        if (il+1) % 100 == 0:
+            print '    ', il+1, '/', len(grouped_lessons), 'lessons'
+
         # only include lessons with a complete set of predictions and at least one fast response
-        if (len(lesson_df) == len(ping_df[ping_df.lesson_id == lesson])) and \
-                lesson_df[actual].values.any():
+        if (len(lesson_df) == len(ping_df[ping_df.lesson_id == lesson])) \
+                and lesson_df[actual].values.any():
             
-            # shuffle indices to randomize pings with equal assigned probabilities
+            # shuffle indices to randomize pings that have 
+            #   equal assigned probabilities
             ind = np.array(range(len(lesson_df)))
             np.random.shuffle(ind)
             sim_ping_order = ind[(-lesson_df[prob].values[ind]).argsort()]
-            #print sim_ping_order
-            
-            #print np.vstack((lesson_df[actual], lesson_df[prob], lesson_df.sec_response < 30.))
-            if not (lesson_df[actual] == (lesson_df.sec_response < 30.)).all():
-                print lesson, np.vstack((lesson_df[ping_id], lesson_df.id, lesson_df[actual], lesson_df.sec_response < 30.))
             
             # indices of pings with < 30 sec response time
             fast_responses_actual = np.where(lesson_df[actual])[0]
-            fast_responses_pred = np.where(lesson_df[actual].values[sim_ping_order])[0]
+            fast_responses_pred = np.where(lesson_df[actual]. \
+                                           values[sim_ping_order])[0]
             
             # number of pings required to get < 30 sec response
-            n_for_fast_response_actual.append(fast_responses_actual.min() + 1)
-            n_for_fast_response_pred.append(fast_responses_pred.min() + 1)
+            n_for_fast_response_actual.append(
+                fast_responses_actual.min() + 1)
+            n_for_fast_response_pred.append(
+                fast_responses_pred.min() + 1)
             
             # number of pings in first 5 with < 30 sec responses
             if len(lesson_df) >= 5:
-                n_fast_in_first_5_actual.append(float(len(np.where(fast_responses_actual < 5)[0])))
-                n_fast_in_first_5_pred.append(float(len(np.where(fast_responses_pred < 5)[0])))
+                n_fast_in_first_5_actual.append(
+                    float(len(np.where(fast_responses_actual < 5)[0])))
+                n_fast_in_first_5_pred.append(
+                    float(len(np.where(fast_responses_pred < 5)[0])))
                 
             # wait time from first ping sent to first response
             def timedelta_with_nan(dt):
                 if np.isnan(dt):
-                    return datetime.timedelta(days=365) # replace NaNs with 1-year response times
+                    # replace NaNs with 1-year response times
+                    return datetime.timedelta(days=365)
                 else:
                     return datetime.timedelta(seconds=dt)
-            sim_time_clicked = np.array([t + timedelta_with_nan(dt) \
-                                         for (t, dt) in zip(lesson_df.time_sent_success.values,
-                                                            lesson_df.sec_response.values[sim_ping_order])])
+            sim_time_clicked = np.array(
+                [t + timedelta_with_nan(dt) for (t, dt) in zip(
+                    lesson_df.time_sent_success.values,
+                    lesson_df.sec_response.values[sim_ping_order])])
             min_time_clicked = lesson_df.time_clicked.dropna().min()
-            if 'float' in str(type(min_time_clicked)):
-                print lesson, lesson_df.time_clicked.values, lesson_df[actual].values, lesson_df.sec_since_online.values
-            else:
-                wait_time_actual.append((lesson_df.time_clicked.dropna().min() - lesson_df.time_sent_success.min()).seconds)
-                wait_time_pred.append((sim_time_clicked.min() - lesson_df.time_sent_success.min()).seconds)
+            wait_time_actual.append(
+                (lesson_df.time_clicked.dropna().min() - \
+                 lesson_df.time_sent_success.min()).seconds)
+            wait_time_pred.append(
+                (sim_time_clicked.min() - \
+                 lesson_df.time_sent_success.min()).seconds)
             
 
 for x in [n_for_fast_response_actual, n_for_fast_response_pred, 
